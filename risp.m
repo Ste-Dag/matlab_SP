d20b2bb58d5db40665f2cb3277b2a1405b7d2b83
Ste-Dag <stefano.dagaro@yahoo.com>
2024-12-01 19:39:13 +0100

    Update risp.m

--- risp.m
+++ risp.m
@@ -1,11 +1,14 @@
-TF=tf([1],[1/(2*pi*100) 1]); %%sistema ordine 1
+% Given the transfer function H(s) = (n1*s +n0)/(d2*s^2 +d1*s +d0)
+% use MATLAB  tf function     H = tf([n1 n0], [d2 d1 d0];   
 
-%%%%%%%%GRADINO%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-step(TF)
+H=tf([1],[1/(2*pi*100) 1]); % first order  
+		       
+%%%%%%%%STEP RESPONSE%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ts=.5;  % time simulated
+step(H,ts)
 
-%%%%%%%%%SINUSOIDE%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t = 0:0.01:4;
-F0=10;
-IN = sin(2*pi*F0*t);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+t = 0:0.01:ts;
+F0=10; IN = sin(2*pi*F0*t);  
 
-lsim(TF,IN,t)
+lsim(H,IN,t)    % use lsim to simulate any input signal 
